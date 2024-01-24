f906c8e401daffb8737bbb1725eafcab37296b47
CeeG33 <ciran.gurbuz@gmail.com>
2024-01-24 18:14:03 +0100

    Masquage des messages d'un utilisateur réussi

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Real Estate',
     'version': '1.0',
-    'category': 'Real Estate',
+    'category': 'Real Estate/Accounting',
     'description': """
 Managing your real estate hasn't been that easy !
 """,
@@ -13,6 +13,7 @@ Managing your real estate hasn't been that easy !
         'security/estate_property_type_views.xml',
         'security/estate_property_tag_views.xml',
         'security/estate_property_offer_views.xml',
+        'security/estate_security.xml',
         'views/estate_menus.xml',
     ],
     'application': True
