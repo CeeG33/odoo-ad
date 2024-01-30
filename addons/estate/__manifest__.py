7b2fffecafc734a0d8f4df6d4cb97493194a99f6
CeeG33 <ciran.gurbuz@gmail.com>
2024-01-30 09:51:13 +0100

    Solution trouvée pour masquage des messages dans le chatter

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Real Estate',
     'version': '1.0',
-    'category': 'Real Estate/Accounting',
+    'category': 'Real Estate',
     'description': """
 Managing your real estate hasn't been that easy !
 """,
